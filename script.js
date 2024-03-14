5cd26d202ce1653447f411daf7996884e18f5c86
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-03-14 16:04:58 -0300

    se crear archivo script.js y se enlaza con index.html

---
+++ script.js
@@ -0,0 +1,16 @@
+function encriptarTexto(texto) {
+    // Reemplazar las letras según las reglas de encriptación
+    texto = texto.replace(/e/g, "enter");
+    texto = texto.replace(/i/g, "imes");
+    texto = texto.replace(/a/g, "ai");
+    texto = texto.replace(/o/g, "ober");
+    texto = texto.replace(/u/g, "ufat");
+
+    return texto;
+}
+
+// Ejemplo de uso
+var textoOriginal = "Hola, cómo estás?";
+var textoEncriptado = encriptarTexto(textoOriginal);
+console.log("Texto original: ", textoOriginal);
+console.log("Texto encriptado: ", textoEncriptado);
